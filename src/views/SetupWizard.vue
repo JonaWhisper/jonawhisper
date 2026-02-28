be226633b87c7e05d849458ac6367f594bc3f871
Jonathan PHILIPPE <jphilippe@fluence.eu>
2026-02-28 12:55:31 +0100

    Adjust Step 2 height to 530 to avoid settings scrollbar

--- src/views/SetupWizard.vue
+++ src/views/SetupWizard.vue
@@ -68,7 +68,7 @@ watch(step, async (newStep) => {
       clearInterval(pollInterval)
       pollInterval = null
     }
-    await win.setSize(new LogicalSize(680, 500))
+    await win.setSize(new LogicalSize(680, 530))
   } else {
     // Resume polling
     if (!pollInterval) {
