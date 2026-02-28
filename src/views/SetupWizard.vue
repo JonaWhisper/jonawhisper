bf845549e47b44a85ec2c02637be1e3f05321819
Jonathan PHILIPPE <jphilippe@fluence.eu>
2026-02-28 12:58:07 +0100

    Increase Step 2 height to 540 to account for actual element sizes

--- src/views/SetupWizard.vue
+++ src/views/SetupWizard.vue
@@ -68,7 +68,7 @@ watch(step, async (newStep) => {
       clearInterval(pollInterval)
       pollInterval = null
     }
-    await win.setSize(new LogicalSize(680, 510))
+    await win.setSize(new LogicalSize(680, 540))
   } else {
     // Resume polling
     if (!pollInterval) {
